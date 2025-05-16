c5765e27e419b2d533113601412d37fbfe0cc4f6
Tejasvita <ttejasvita@gmail.com>
2025-05-16 17:10:04 +0530

    2900: removed vector 'vec' for storing indices

--- 2900.cpp
+++ 2900.cpp
@@ -5,20 +5,15 @@ using namespace std;
 class Solution {
 public:
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        vector<int> vec;
         vector<string> ans;
 
         int index = 0;
-        vec.push_back(index);
+        ans.push_back(words[index]);
         for (int i = 0; i < words.size(); i++) {
             if (groups[i] != groups[index]) {
                 index = i;
-                vec.push_back(index);
+                ans.push_back(words[index]);
             }
-        }
-
-        for (int val : vec) {
-            ans.push_back(words[val]);
         }
 
         return ans;
