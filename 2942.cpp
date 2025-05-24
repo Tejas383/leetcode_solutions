95e32c3145064c89069a7d2fcf8387173e4bef60
Tejasvita <ttejasvita@gmail.com>
2025-05-24 11:59:52 +0530

    2942: Approach: Built-in String Search, using find() function

--- 2942.cpp
+++ 2942.cpp
@@ -7,11 +7,9 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words[i].length(); j++) {
-                if (words[i][j] == x) {
-                    ans.push_back(i);
-                    break;
-                }
+            if (words[i].find(x) != string::npos) {
+                ans.push_back(i);
+                break;
             }
         }
         return ans;
