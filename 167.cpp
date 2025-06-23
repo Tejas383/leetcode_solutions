e63a4ec39fb4e714ed836d5f65963f3a9318e284
Tejasvita <ttejasvita@gmail.com>
2025-06-23 19:30:59 +0530

    167: using Two-Pointer approach

--- 167.cpp
+++ 167.cpp
@@ -5,10 +5,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        for (int i = 0; i < numbers.size(); i++)
-            for (int j = i + 1; j < numbers.size(); j++)
-                if (numbers[i] + numbers[j] == target)
-                    return {i + 1, j + 1};
-        return {-1, -1};
+        vector<int> ans;
+        int i = 0;
+        int j = numbers.size() - 1;
+        while (j > i) {
+            int sum = numbers[i] + numbers[j];
+            if (sum > target) j--;
+            else if (sum < target) i++;
+            else {
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                break;
+            } 
+        }
+        return ans;
     }
 };
