f5cb150c772e49a66dd688b92818f92b4ee8cebc
Tejasvita <ttejasvita@gmail.com>
2025-06-26 22:49:59 +0530

    2149: using Index Mapping approach (with odd and even indices) - placing elements directly at specific indices

---
+++ 2149.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(), 0);
+        int p = 0;
+        int n = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                ans[p] = nums[i];
+                p += 2;
+            } else {
+                ans[n] = nums[i];
+                n += 2;
+            }
+        }
+
+        return ans;
+    }
+};
