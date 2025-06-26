83968935a4b1534b2afd38ff4701be033f23673c
Tejasvita <ttejasvita@gmail.com>
2025-06-26 14:52:48 +0530

    540: using Binary Search Approach

--- 540.cpp
+++ 540.cpp
@@ -4,9 +4,28 @@ using namespace std;
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        for (int i = 0; i < nums.size(); i+=2) {
-            if (nums[i] != nums[i+1]) return nums[i];
+        int l = 0;
+        int h = nums.size() - 1;
+        int m;
+        while (true) {
+            m = (l + h) / 2;
+            if (m % 2 == 0) {
+                if (m != 0 && nums[m - 1] == nums[m]) {
+                    h = m;
+                } else if (m != nums.size()-1 && nums[m + 1] == nums[m]) {
+                    l = m;
+                } else {
+                    return nums[m];
+                }
+            } else {
+                if (m != nums.size()-1 && nums[m + 1] == nums[m]) {
+                    h = m - 1;
+                } else if (m != 0 && nums[m - 1] == nums[m]) {
+                    l = m + 1;
+                } else {
+                    return nums[m];
+                }
+            }
         }
         return 0;
     }
