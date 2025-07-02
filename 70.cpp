e1f3a91e770dfab60c23ddbf56a84c48c2597e0b
Tejasvita <ttejasvita@gmail.com>
2025-07-02 11:30:09 +0530

    70: using Fibonacci Series: number of ways to reach step n = sum of number of ways to reach the previous two steps T(n) = T(n - 1) + T(n - 2)

---
+++ 70.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a = 0;
+        int b = 1;
+        int c;
+        int i = 0;
+        while (i < n) {
+            c = a + b;
+            a = b;
+            b = c;
+            i++;
+        }
+        return c;
+    }
+};
