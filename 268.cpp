5b5c13b0a2cbc8f8d63c766f5513663cd81b724e
Tejasvita <ttejasvita@gmail.com>
2025-07-05 15:33:53 +0530

    268: using Cyclic Sort method

--- 268.cpp
+++ 268.cpp
@@ -4,12 +4,17 @@ using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int calcSum = 0;
-        int n = nums.size();
-        int expSum = n * (n + 1) / 2;
-        for (auto i : nums) {
-            calcSum += i;
+        int i = 0;
+        while (i < nums.size()) {
+            if (nums[i] < nums.size() && nums[i] != nums[nums[i]]) 
+                swap(nums[i], nums[nums[i]]);
+            else 
+                i++;
         }
-        return expSum - calcSum;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != j)
+                return j;
+        }
+        return nums.size();
     }
 };
