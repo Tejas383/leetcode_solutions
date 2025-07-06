057f7d7714c9f4c5a2e3cdaf03ca9a3e9ab598f0
Tejasvita <ttejasvita@gmail.com>
2025-07-06 17:41:41 +0530

    215: added alternate approach - quickSelect algorithm, not so optimum

--- 215.cpp
+++ 215.cpp
@@ -13,3 +13,36 @@ public:
         return nums.front();
     }
 };
+
+// we are a=not using the quickSelect algorithm as given below, since
+// the worst case time complexity of quickSelect is O(N^2)
+// while that of heap is O(n.logn)
+
+// class Solution {
+// public:
+//     void quickSort(vector<int>& arr, int start, int end, int index) {
+//         int pivot = arr[end];
+//         int i = start;
+
+//         for (int j = start; j < end; j++) {
+//             if (arr[j] < pivot) {
+//                 swap(arr[i], arr[j]);
+//                 i++;
+//             }
+//         }
+//         swap(arr[i], arr[end]);
+
+//         if (i == (index)) return;
+
+//         else if (index < i) 
+//             quickSort(arr, start, i - 1, index);
+//         else 
+//             quickSort(arr, i + 1, end, index);
+//     }
+
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int index = nums.size() - k;
+//         quickSort(nums, 0, nums.size() - 1, index);
+//         return nums[index];
+//     }
+// };
