c8b5c151e8ed0209d1c1bf9faf887cc8b3345a7f
Tejasvita <ttejasvita@gmail.com>
2025-07-09 15:36:33 +0530

    1: using Two-Pass Hash Map

--- 1.cpp
+++ 1.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans(2, -1);
-
+        unordered_map<int, int> mp;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans[0] = i;
-                    ans[1] = j;
-                }
-            }
+            mp[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int comp = target - nums[i];
+            if (mp.count(comp) && (mp[comp] != i))
+                return {i, mp[comp]};
         }
 
-        return ans;
+        return {};
     }
 };
