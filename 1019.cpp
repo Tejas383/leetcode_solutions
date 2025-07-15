4996135a62f49f4acc426687c3a926c759f9f550
Tejasvita <ttejasvita@gmail.com>
2025-07-15 11:33:41 +0530

    1019: using Decreasing Monotonic Stack

---
+++ 1019.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> nodes;
+        ListNode* temp = head;
+        while (temp) {
+            nodes.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        int n = nodes.size();
+        vector<int> nge(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nodes[i] >= st.top())
+                st.pop();
+            
+            nge[i] = st.empty() ? 0 : st.top();
+            st.push(nodes[i]);
+        }
+
+        return nge;
+    }
+};
