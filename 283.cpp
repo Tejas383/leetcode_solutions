d7834623570fe2cd01670233fcdfe96f564b11d1
Tejasvita <ttejasvita@gmail.com>
2025-07-21 14:23:44 +0530

    283: using Compaction to shift non-zeros forward, then Fill remaining indices with 0s

--- 283.cpp
+++ 283.cpp
@@ -3,18 +3,12 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        int j = 0;
-        while (j < nums.size()) {
-            while (i < j && nums[i] != 0) 
-                i++;
-            while (j < nums.size() - 1 && nums[j] == 0)
-                j++;
-            
-            if (nums[i] == 0 && nums[j] != 0)
-                swap(nums[i++], nums[j]);
-            
-            j++;
-        }
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != 0)
+                nums[pos++] = nums[i];
+
+        while (pos < nums.size())
+            nums[pos++] = 0;
     }
 };
