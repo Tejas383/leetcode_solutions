188f77d11756593eb6a5fc7d676b730611ab0303
Tejasvita <ttejasvita@gmail.com>
2025-07-21 11:04:24 +0530

    189: using In-Place Reversal approach

--- 189.cpp
+++ 189.cpp
@@ -4,16 +4,10 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if (k >= nums.size())
-            k %= nums.size();
+        k %= nums.size();
         
-        vector<int> ans;
-        for (int i = nums.size() - k; i < nums.size(); i++) {
-            ans.push_back(nums[i]);
-        }
-        for (int i = 0; i < nums.size() - k; i++) {
-            ans.push_back(nums[i]);
-        }
-        nums = ans;
+        reverse(nums.begin(), nums.end() - k);
+        reverse(nums.end() - k, nums.end());
+        reverse(nums.begin(), nums.end());
     }
 };
