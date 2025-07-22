187af69744a8338672e776eb1ff705902b88f11c
Tejasvita <ttejasvita@gmail.com>
2025-07-22 14:29:10 +0530

    162: using Binary Search

--- 162.cpp
+++ 162.cpp
@@ -4,13 +4,21 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if (nums.size() == 1) return 0;
-        if (nums[0] > nums[1]) return 0;
-        for (int i = 1; i < nums.size() - 1; i++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) 
-                return i;
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int left = (mid == 0) ? INT_MIN : nums[mid - 1];
+            int right = (mid == nums.size() - 1) ? INT_MIN : nums[mid + 1];
+
+            if (left < nums[mid] && right < nums[mid]) 
+                return mid;
+            if (right > nums[mid]) 
+                low = mid + 1;
+            else 
+                high = mid - 1;
         }
-        if (nums[nums.size() - 1] > nums[nums.size() - 2]) return (nums.size() - 1);
-        return -1;
+        return 0;
     }
 };
