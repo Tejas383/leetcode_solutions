04dbdce4e0dd28ebe4b648bdcdb7ea079ddcfe3f
Tejasvita <ttejasvita@gmail.com>
2025-07-22 11:36:05 +0530

    169: using Sorting-Based approach: the middle element will always be present in majority

--- 169.cpp
+++ 169.cpp
@@ -4,12 +4,7 @@ using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> count;
-        int n = nums.size() / 2;
-        for (int el : nums) {
-            count[el]++;
-            if (count[el] > n) return el;
-        }
-        return 0;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
     }
 };
