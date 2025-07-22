ba5ac7c7ed4cb5398726fb66d79bf31c7ad08604
Tejasvita <ttejasvita@gmail.com>
2025-07-22 11:33:44 +0530

    169: using Hash-Map to store Frequency count

--- 169.cpp
+++ 169.cpp
@@ -4,17 +4,11 @@ using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-        
+        unordered_map<int, int> count;
         int n = nums.size() / 2;
-        sort(nums.begin(), nums.end());
-        int count = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1]) count++;
-            else count = 1;
-
-            if (count > n) return nums[i];
+        for (int el : nums) {
+            count[el]++;
+            if (count[el] > n) return el;
         }
         return 0;
     }
