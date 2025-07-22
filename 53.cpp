ece02091d7918784ca9ce08a378502ead3326c81
Tejasvita <ttejasvita@gmail.com>
2025-07-22 10:15:30 +0530

    53: Kadane's Algorithm explained

--- 53.cpp
+++ 53.cpp
@@ -2,16 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Kadane's algorithm
+// KADANE'S ALGORITHM:
+// Add each element to sum, update answer with max(sum, answer), 
+// reset sum to 0 if it's negative (as it can't contribute to future maximum).
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int ans = INT_MIN;
         int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if (sum > ans) ans = sum;
-            if (sum < 0) sum = 0; 
+        for (int el : nums) {
+            sum += el;
+            ans = max(sum, ans);
+            sum = max(sum, 0);
         }
         return ans;
     }
