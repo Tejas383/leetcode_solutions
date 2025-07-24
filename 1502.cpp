edbf35d832d287935ff73f4fef29e1a902e2e4f4
Tejasvita <ttejasvita@gmail.com>
2025-07-24 10:51:45 +0530

    1502: Math + Hashing solution to Check if array can form arithmetic progression Without Sorting

--- 1502.cpp
+++ 1502.cpp
@@ -4,12 +4,33 @@ using namespace std;
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int diff = arr[1] - arr[0];
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr[i] - arr[i - 1] != diff)
-                return false;
+        int n = arr.size();
+        int maxEl = *max_element(arr.begin(), arr.end());
+        int minEl = *min_element(arr.begin(), arr.end());
+
+        if ((maxEl - minEl) % (n - 1) != 0)
+            return false;
+        
+        int d = (maxEl - minEl) / (n - 1);
+        if (d == 0) {
+            for (int el : arr) {
+                if (el != minEl)
+                    return false;
+            }
+            return true;
         }
+
+        vector<bool> pres(n, false);
+        for (int el : arr) {
+            if ((el - minEl) % d != 0) {
+                return false; 
+            }
+            int pos = (el - minEl) / d;
+            if (pos >= n || pres[pos])
+                return false;
+            pres[pos] = true;
+        }
+
         return true;
     }
 };
