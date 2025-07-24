8a485900b08d349060dab71804aace078adb0e6a
Tejasvita <ttejasvita@gmail.com>
2025-07-24 09:50:43 +0530

    1413: using Prefix-Sum + Minimum Tracking approach (with no extra space)

--- 1413.cpp
+++ 1413.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        vector<int> prefix(nums.size() + 1);
-        prefix[0] = 0;
+        int sum = 0;
+        int minEl = 0;
 
-        for (int i = 1; i <= nums.size(); i++) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
+        for (int el : nums) {
+            sum += el;
+            minEl = min(sum, minEl);
         }
-        int min = *min_element(prefix.begin(), prefix.end());
 
-        return (min <= 0) ? abs(min) + 1 : min;
+        return 1 - minEl;
     }
 };
