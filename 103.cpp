d6cf9c8f75b1175f96a878bc9798d936aaaca329
Tejasvita <ttejasvita@gmail.com>
2025-07-28 21:17:34 +0530

    103: using BFS (Breadth-First Search) with a zigzag Flag to alternate direction on each level.

---
+++ 103.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) 
+            return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag = false;
+
+        while(!q.empty()) {
+            vector<int> level;
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                if (flag)
+                    level.insert(level.begin(), q.front()->val);
+                else
+                    level.push_back(q.front()->val);
+                if (q.front()->left)
+                    q.push(q.front()->left);
+                if (q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+            flag = !flag;
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
