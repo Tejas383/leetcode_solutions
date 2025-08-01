750b1d070a0d4284399c6135859756049b53e15e
Tejasvita <ttejasvita@gmail.com>
2025-08-01 11:34:56 +0530

    118: using Iterative Bottom-Up Dynamic Programming approach

---
+++ 118.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal(numRows);
+        for (int i = 0; i < numRows; i++) {
+            pascal[i].resize(i + 1);
+            pascal[i][0] = 1;
+            pascal[i][i] = 1;
+            for (int j = 1; j < pascal[i].size() - 1; j++)
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+        return pascal;
+    }
+};
