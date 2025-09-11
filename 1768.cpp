b48bb51ed093d635db2bf1bd23d2293e71f93b04
Tejasvita <ttejasvita@gmail.com>
2025-09-11 12:35:15 +0530

    1768: using 2-Pointer approach in a Single Loop

--- 1768.cpp
+++ 1768.cpp
@@ -6,15 +6,11 @@ public:
     string mergeAlternately(string word1, string word2) {
         string merged = "";
         int i = 0, j = 0;
-        while (i < word1.length() && j < word2.length()) {
-            merged += word1[i++];
-            merged += word2[j++];
-        }
-        while (i < word1.length()) {
-            merged += word1[i++];
-        }
-        while (j < word2.length()) {
-            merged += word2[j++];
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length())
+                merged += word1[i++];
+            if (j < word2.length())
+                merged += word2[j++];
         }
         return merged;
     }
