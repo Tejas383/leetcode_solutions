b898b8a56007cbfd3169592f49238ed489eaa7c6
Tejasvita <ttejasvita@gmail.com>
2025-09-15 01:46:46 +0530

    1431: using Two-Pass approach

---
+++ 1431.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result(candies.size());
+        int maxi = 0;
+        for (int i = 0; i < candies.size(); i++) {
+            maxi = max(maxi, candies[i]);
+        }
+        for (int i = 0; i < candies.size(); i++) {
+            if (candies[i] + extraCandies >= maxi) 
+                result[i] = true;
+            else 
+                result[i] = false;
+        }
+        return result;
+    }
+};
