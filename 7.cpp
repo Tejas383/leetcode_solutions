bf3cd53fd87232864e7a398eb23f7f1e0352073a
Tejas383 <ttejasvita@gmail.com>
2025-10-13 20:11:39 +0530

    7: using Mathematical Digit-Extraction and Reconstruction method

--- 7.cpp
+++ 7.cpp
@@ -6,14 +6,13 @@ public:
     int reverse(int x) {
         int rev = 0;
         while (x != 0) {
-            int append = x % 10;
-            x /= 10;
-
-            if ((rev > INT_MAX / 10) || (rev == INT_MAX / 10 && append > 7))
+            int temp = x;
+        int rev = 0;
+        while (temp != 0) {
+            if (rev < INT_MIN / 10 || rev > INT_MAX / 10) 
                 return 0;
-            if ((rev < INT_MIN / 10) || (rev == INT_MIN / 10 && append < -8))
-                return 0;
-            rev = (rev * 10)  + append;
+            rev = (rev * 10) + (temp % 10);
+            temp /= 10;
         }
         return rev;
     }
