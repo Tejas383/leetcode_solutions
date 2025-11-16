7e48bb1a480efae5545469f96c6f275e5d7b632e
Tejasvita <ttejasvita@gmail.com>
2025-11-16 23:18:32 +0530

    237: using Loop

---
+++ 237.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        while (node->next->next) {
+            node->val = node->next->val;
+            node = node->next;
+        }
+        node->val = node->next->val;
+        node->next = NULL;
+    }
+};
