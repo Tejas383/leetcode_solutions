d1e7841253c1cdb4456a2c4099a6a3a2f0041d4d
Tejasvita <ttejasvita@gmail.com>
2025-12-26 22:16:30 +0530

    680: using Two-Pointer approach (with at most One Deletion / Greedy Check)

---
+++ 680.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    bool checkPalindrome(string s, int i, int j) {
+        while (i < j) {
+            if (s[i] == s[j])
+                i++, j--;
+            else 
+                return false;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        while (i < j) {
+            if (s[i] == s[j])
+                i++, j--;
+            else {
+                return (checkPalindrome(s, i, j - 1) || checkPalindrome(s, i + 1, j));
+            }
+        }
+        return true;
+    }
+};
