03add13e989e954486d967a1378fa3a06e34d587
Tejasvita <ttejasvita@gmail.com>
2025-12-26 14:13:14 +0530

    2483: using Greedy prefix minimum (Kadane-style penalty delta) approach

--- 2483.cpp
+++ 2483.cpp
@@ -3,36 +3,19 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int idx;
-        int len = customers.size();
+        int idx = 0;
+        int curr_pen = 0;
+        int min_pen = 0;
 
-        string rev = customers;
-        reverse(rev.begin(), rev.end());
-
-        vector<int> preN;
-        preN.push_back(0);
-        vector<int> sufY;
-        sufY.push_back(0);
-
-        for (int i = 1; i <= len; i++) {
-            if (customers[i - 1] == 'N')
-                preN.push_back(preN[i - 1] + 1);
+        for (int i = 1; i <= customers.size(); i++) {
+            if (customers[i - 1] == 'Y')
+                curr_pen--;
             else 
-                preN.push_back(preN[i - 1]);
+                curr_pen++;
 
-            if (rev[i - 1] == 'Y')
-                sufY.push_back(sufY[i - 1] + 1);
-            else 
-                sufY.push_back(sufY[i - 1]);
-        }
-
-        reverse(sufY.begin(), sufY.end());
-
-        int minVal = INT_MAX;
-        for (int i = 0; i <= len; i++) {
-            if ((preN[i] + sufY[i]) < minVal) {
-                minVal = preN[i] + sufY[i];
+            if (curr_pen < min_pen) {
                 idx = i;
+                min_pen = curr_pen;
             }
         }
 
