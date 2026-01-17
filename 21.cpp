75abdbf157bd043ad828427dbddf20130f4a121b
Tejasvita <ttejasvita@gmail.com>
2026-01-17 20:20:50 +0530

    21: using Iterative In-Place Two-Pointer Merge approach

--- 21.cpp
+++ 21.cpp
@@ -9,38 +9,30 @@ public:
             return list1;
         
         ListNode* list = nullptr;
-        ListNode* temp = list;
-        while (list1 && list2) {
-            int sm;
-            if (list1->val < list2->val) {
-                sm = list1->val;
-                list1 = list1->next;
-            } else {
-                sm = list2->val;
-                list2 = list2->next;
-            }
-
-            ListNode* node = new ListNode(sm);
-            if (!list) {
-                list = node;
-                temp = list;
-            } else {
-                temp->next = node;
-                temp = temp->next;
-            }
-        }
-
-        while (list1) {
-            temp->next = list1;
-            temp = temp->next;
+        if (list1->val < list2->val) {
+            list = list1;
             list1 = list1->next;
-        }
-
-        while (list2) {
-            temp->next = list2;
-            temp = temp->next;
+        } else {
+            list = list2;
             list2 = list2->next;
         }
+
+        ListNode* temp = list;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        if (list1) 
+            temp->next = list1;
+        if (list2)
+            temp->next = list2;
 
         return list;
     }
