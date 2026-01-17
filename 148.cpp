b0b0510be5a11bf12cb1d6bd004a8bd08dd0d2cb
Tejasvita <ttejasvita@gmail.com>
2026-01-17 19:40:19 +0530

    148: using Array-Based Sorting approach (Convert Linked List to Array and Sort)

---
+++ 148.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp = head;
+        if (!temp || !temp->next)
+            return head;
+        
+        while (temp) {
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        sort(arr.begin(), arr.end());
+        temp = head;
+        int i = 0;
+        while (temp) {
+            temp->val = arr[i++];
+            temp = temp->next;
+        }
+        return head;
+    }
+};
