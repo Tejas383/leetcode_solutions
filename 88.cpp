a5a79debb340f75c322045396211691808195ff9
Tejasvita <ttejasvita@gmail.com>
2026-01-20 13:58:57 +0530

    88: using Two-Pointer approach (from the end) Time Complexity = O(m + n) Space Complexity = O(1)

--- 88.cpp
+++ 88.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
-        int j = 0;
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j])
-                i++;
-            else {
-                for (int k = m; k > i; k--)
-                    nums1[k] = nums1[k - 1];
-                nums1[i] = nums2[j];
-                j++;
-                m++;
-            }
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j])
+                nums1[k] = nums1[i--];
+            else 
+                nums1[k] = nums2[j--];
+            k--;
         }
-        while (i < m + n && j < n)
-            nums1[i++] = nums2[j++];
+
+        if (i < 0) {
+            while (j >= 0)
+                nums1[k--] = nums2[j--];
+        }
     }
 };
