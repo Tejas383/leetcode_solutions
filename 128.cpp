25425095fb3de2bb2fa849ddbad1e746d2f34d39
Tejasvita <ttejasvita@gmail.com>
2026-01-22 11:26:37 +0530

    128: using Hash Set–Based Linear Scan (Sequence-Start Detection) approach

--- 128.cpp
+++ 128.cpp
@@ -3,28 +3,25 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        set<int> s;
-        for (int num : nums) 
-            s.insert(num);
-        
+        unordered_set<int> s(nums.begin(), nums.end());
+
         int maxLen = 0;
-        int len = 0;
-        int prev = INT_MIN;
-        bool flag = false;
-        for (auto el : s) {
-            if (el == prev + 1) {
-                len++;
+
+        for (int num : s) {
+            // start only if num is the beginning of a sequence
+            if (s.find(num - 1) == s.end()) {
+                int curr = num;
+                int len = 1;
+
+                while (s.find(curr + 1) != s.end()) {
+                    curr++;
+                    len++;
+                }
+
                 maxLen = max(maxLen, len);
-            } else {
-                len = 0;
-                flag = true;
             }
-            prev = el;
         }
 
-        if (flag)
-            maxLen++;
-        
         return maxLen;
     }
 };
