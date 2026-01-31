db2fa183ee97553c55323d87328fc220a28bef12
Tejasvita <ttejasvita@gmail.com>
2026-01-31 09:55:15 +0530

    779: using Recursion (Top-Down Divide and Conquer) approach
    Time Complexity = O(n)
    Space Complexity = O(n)

---
+++ 779.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if (n == 1)
+            return 0;
+        
+        if (k % 2 == 1) {
+            return kthGrammar(n - 1, (k + 1) / 2);
+        } else {
+            return !kthGrammar(n - 1, (k + 1) / 2);
+        }
+    }
+};
