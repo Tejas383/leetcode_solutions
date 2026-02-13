2df1947a9e8e545a613197da811a99edb161a196
Tejasvita <ttejasvita@gmail.com>
2026-02-13 10:52:45 +0530

    322: using Tabulation
    Time Complexity = O(n * amount)
    Time Complexity = O(n * amount)

--- 322.cpp
+++ 322.cpp
@@ -2,25 +2,32 @@
 
 class Solution {
 public:
-    int helper(vector<int>& coins, int amount, int n, vector<vector<int>>& dp) {
-        if (dp[n][amount] != -1)
-            return dp[n][amount];
-
-        if (n == 0)
-            return dp[n][amount] = 1e9;
-        
-        if (amount == 0)
-            return dp[n][amount] = 0;
-        
-        if (coins[n - 1] <= amount)
-            return dp[n][amount] = min(1 + helper(coins, amount - coins[n - 1], n, dp), helper(coins, amount, n - 1, dp));
-        
-        return dp[n][amount] = helper(coins, amount, n - 1, dp);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount + 1, -1));
-        int ans = helper(coins, amount, coins.size(), dp);
-        return ans >= 1e9 ? -1 : ans;
+        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount + 1));
+
+        // we can not make a particular amount if number of coins is 0
+        // therefore, the number of coins is set to max
+        // MAX_INT - 1 -> to control integer overflow
+        for (int j = 0; j < dp[0].size(); j++)
+            dp[0][j] = INT_MAX - 1;
+        
+        // to create the amount of 0, we donot need any coins
+        // therefore, the whole column is 0
+        for (int i = 0; i < dp.size(); i++) 
+            dp[i][0] = 0;
+        
+        for (int i = 1; i < dp.size(); i++) {
+            for (int j = 1; j < dp[i].size(); j++) {
+                if (coins[i - 1] <= j)
+                    dp[i][j] = min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+                else 
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        if (dp[coins.size()][amount] >= INT_MAX - 1) 
+            return -1;
+
+        return dp[coins.size()][amount];
     }
 };
