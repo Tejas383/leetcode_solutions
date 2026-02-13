eae6afab4acfa21e2911202b11e99374f310c713
Tejasvita <ttejasvita@gmail.com>
2026-02-13 09:57:53 +0530

    322: using Memoisation
    Time complexity = O(n * amount)
    Space complexitiy = O(n * amount)

--- 322.cpp
+++ 322.cpp
@@ -2,21 +2,25 @@
 
 class Solution {
 public:
-    int helper(vector<int>& coins, int amount, int n) {
+    int helper(vector<int>& coins, int amount, int n, vector<vector<int>>& dp) {
+        if (dp[n][amount] != -1)
+            return dp[n][amount];
+
         if (n == 0)
-            return 1e9;
+            return dp[n][amount] = 1e9;
         
         if (amount == 0)
-            return 0;
+            return dp[n][amount] = 0;
         
         if (coins[n - 1] <= amount)
-            return min(1 + helper(coins, amount - coins[n - 1], n), helper(coins, amount, n - 1));
+            return dp[n][amount] = min(1 + helper(coins, amount - coins[n - 1], n, dp), helper(coins, amount, n - 1, dp));
         
-        return helper(coins, amount, n - 1);
+        return dp[n][amount] = helper(coins, amount, n - 1, dp);
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        int ans = helper(coins, amount, coins.size());
+        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount + 1, -1));
+        int ans = helper(coins, amount, coins.size(), dp);
         return ans >= 1e9 ? -1 : ans;
     }
 };
