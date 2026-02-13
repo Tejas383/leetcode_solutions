5010aa6e3d7262d4aedbbaae73ea6d241ba4f4e5
Tejasvita <ttejasvita@gmail.com>
2026-02-13 10:57:31 +0530

    1143: Optimisation: using Tabulation
    Time complexity = O(n * m)
    Space complexity = O(n * m)

--- 1143.cpp
+++ 1143.cpp
@@ -2,22 +2,18 @@
 
 class Solution {
 public:
-    int lcs(string& str1, int n, string& str2, int m, vector<vector<int>>& dp) {
-        if (dp[n][m] != -1)
-            return dp[n][m];
-
-        if (n == 0 || m == 0)
-            return 0;
-        
-        if (str1[n - 1] == str2[m - 1])
-            return dp[n][m] = (1 + lcs(str1, n - 1, str2, m - 1, dp));
-        
-        return dp[n][m] = max(lcs(str1, n - 1, str2, m, dp), lcs(str1, n, str2, m - 1, dp));
-    }
-
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size() + 1, vector<int> (text2.size() + 1, -1));
+        vector<vector<int>> dp(text1.size() + 1, vector<int> (text2.size() + 1, 0));
 
-        return lcs(text1, text1.size(), text2, text2.size(), dp);
+        for (int i = 1; i < dp.size(); i++) {
+            for (int j = 1; j < dp[0].size(); j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else 
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[text1.size()][text2.size()];
     }
 };
