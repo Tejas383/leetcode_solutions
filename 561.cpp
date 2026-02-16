2fa1ffa8f112dfe1a1f1a8d9a92b1a54f2701306
Tejasvita <ttejasvita@gmail.com>
2026-02-16 10:15:15 +0530

    561: using Recursion
    Time Complexty = O(2 ^ n)
    Space Complexity = O(n)
    
    (TLE)

--- 561.cpp
+++ 561.cpp
@@ -2,14 +2,21 @@
 
 class Solution {
 public:
-    int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i += 2) {
-            sum += nums[i];
+    int lcs(string str, int n, string rev, int m) {
+        if (n == 0 || m == 0)
+            return 0;
+        
+        if (str[n - 1] == rev[m - 1]) {
+            return (1 + lcs(str, n - 1, rev, m - 1));
         }
+        return max(lcs(str, n - 1, rev, m), lcs(str, n, rev, m - 1));
+    }
 
-        return sum;
+    int longestPalindromeSubseq(string s) {
+        string str = s;
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        return lcs(str, str.size(), rev, rev.size());
     }
 };
