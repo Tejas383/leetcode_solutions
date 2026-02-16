ebaf382081aceb4e3b816a9d7cf3623487e582ed
Tejasvita <ttejasvita@gmail.com>
2026-02-16 14:35:08 +0530

    70: optimisation using Memoisation (Top-Down DP)
    Time Complexity = O(n)
    Space Complexity = O(n) + O(n)
    (recursion stack + dp)

--- 70.cpp
+++ 70.cpp
@@ -2,23 +2,25 @@
 
 class Solution {
 public:
-    int solve(int n) {
-        if (n == 0)
-            return 1;
+    int solve(int n, vector<int>& dp) {
+        if (dp[n] != -1)
+            return dp[n];
         
-        if (n == 1)
-            return 1;
+        if (n == 0 || n == 1)
+            return dp[n] = 1;
         
         // take one step
-        int one = solve(n - 1);
+        int one = solve(n - 1, dp);
 
         // take two steps
-        int two = solve(n - 2);
+        int two = solve(n - 2, dp);
 
-        return one + two;
+        return dp[n] = one + two;
     }
 
     int climbStairs(int n) {
-        return solve(n);
+        vector<int> dp(n + 1, -1);
+
+        return solve(n, dp);
     }
 };
