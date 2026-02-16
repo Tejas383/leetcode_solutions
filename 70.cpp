c41db12e0f2f2691370635216d763221fdb0f07e
Tejasvita <ttejasvita@gmail.com>
2026-02-16 14:35:59 +0530

    70: optimised using Tabulation
    Time complexity = O(n)
    Space complexity = O(n)

--- 70.cpp
+++ 70.cpp
@@ -2,25 +2,13 @@
 
 class Solution {
 public:
-    int solve(int n, vector<int>& dp) {
-        if (dp[n] != -1)
-            return dp[n];
-        
-        if (n == 0 || n == 1)
-            return dp[n] = 1;
-        
-        // take one step
-        int one = solve(n - 1, dp);
-
-        // take two steps
-        int two = solve(n - 2, dp);
-
-        return dp[n] = one + two;
-    }
-
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 1);
 
-        return solve(n, dp);
+        for (int i = 2; i < dp.size(); i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
