74aa51561db00e267784bf05c700abafe4003481
Tejasvita <ttejasvita@gmail.com>
2026-02-17 19:57:08 +0530

    300: Optimisation using Top-Down DP (Memoisation)
    Time complexity = O(n^2)
    Space complexity= O(n^2)

--- 300.cpp
+++ 300.cpp
@@ -2,19 +2,23 @@
 
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int prevIdx) {
+    int solve(vector<int>& nums, int i, int prevIdx, vector<vector<int>>& dp) {
+        if (dp[i][prevIdx + 1] != -1)
+            return dp[i][prevIdx + 1];
+
         if (i == nums.size())
-            return 0;
+            return dp[i][prevIdx + 1] = 0;
         
         int take = 0;
         if (prevIdx == -1 || nums[i] > nums[prevIdx])
-            take = 1 + solve(nums, i + 1, i);
-        int skip = solve(nums, i + 1, prevIdx);
+            take = 1 + solve(nums, i + 1, i, dp);
+        int skip = solve(nums, i + 1, prevIdx, dp);
 
-        return max(take, skip);
+        return dp[i][prevIdx + 1] = max(take, skip);
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        return solve(nums, 0, -1);
+        vector<vector<int>> dp(nums.size() + 1, vector<int> (nums.size() + 1, -1));
+        return solve(nums, 0, -1, dp);
     }
 };
