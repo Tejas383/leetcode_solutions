b828e428315c5fc7067b6f420089f380e0000f2c
Tejasvita <ttejasvita@gmail.com>
2026-02-18 11:54:22 +0530

    1691: optimised using Memoisation
    Time complexity = O(n^2)
    Space complexity = O(n^2)

--- 1691.cpp
+++ 1691.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& cuboids, int curr, int prev) {
+    int solve(vector<vector<int>>& cuboids, int curr, int prev, vector<vector<int>>& dp) {
+        if (dp[curr][prev + 1] != -1)
+            return dp[curr][prev + 1];
+
         if (curr == cuboids.size())
-            return 0;
+            return dp[curr][prev + 1] = 0;
         
         int take = 0;
         if (prev == -1 || 
@@ -10,10 +13,10 @@ public:
           && cuboids[curr][1] >= cuboids[prev][1]
           && cuboids[curr][2] >= cuboids[prev][2])
          )
-            take = cuboids[curr][2] + solve(cuboids, curr + 1, curr);
-        int notTake = solve(cuboids, curr + 1, prev);
+            take = cuboids[curr][2] + solve(cuboids, curr + 1, curr, dp);
+        int notTake = solve(cuboids, curr + 1, prev, dp);
 
-        return max(take, notTake);
+        return dp[curr][prev + 1] = max(take, notTake);
     }
 
     int maxHeight(vector<vector<int>>& cuboids) {
@@ -22,6 +25,8 @@ public:
         
         sort(cuboids.begin(), cuboids.end());
 
-        return solve(cuboids, 0, -1);
+        vector<vector<int>> dp(cuboids.size() + 1, vector<int> (cuboids.size() + 1, -1));
+
+        return solve(cuboids, 0, -1, dp);
     }
 };
