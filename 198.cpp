17a7a8f3562b6a9dcac601d7b5362a0ee9ecb3d4
Tejasvita <ttejasvita@gmail.com>
2026-02-19 11:19:32 +0530

    198: optimisation using Tabulation
    time complexity = O(n)
    space complexity = O(n)

--- 198.cpp
+++ 198.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int n, vector<int>& dp) {
-        if (dp[n] != -1) 
-            return dp[n];
-        
-        if (n == 0)
-            return dp[n] = 0;
-
-        if (n == 1)
-            return dp[n] = nums[0];
-        
-        int take = nums[n - 1] + solve(nums, n - 2, dp);
-        int notTake = solve(nums, n - 1, dp);
-
-        return dp[n] = max(take, notTake);
-    } 
-
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size() + 1, -1);
+        vector<int> dp(nums.size() + 1, 0);
+        dp[1] = nums[0];
 
-        return solve(nums, nums.size(), dp);
+        for (int i = 2; i < dp.size(); i++)
+            dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+
+        return dp[nums.size()];
     }
 };
