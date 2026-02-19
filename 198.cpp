9130dbe129b034a1d0ffef8675da859153e5a059
Tejasvita <ttejasvita@gmail.com>
2026-02-19 10:25:24 +0530

    198: optimisation using Memoisation
    Time complexity = O(n)
    Space complexity = O(n) + O(n)
    (dp vector + recursion stack)

--- 198.cpp
+++ 198.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int n) {
+    int solve(vector<int>& nums, int n, vector<int>& dp) {
+        if (dp[n] != -1) 
+            return dp[n];
+        
         if (n == 0)
-            return 0;
+            return dp[n] = 0;
 
         if (n == 1)
-            return nums[0];
+            return dp[n] = nums[0];
         
-        int take = nums[n - 1] + solve(nums, n - 2);
+        int take = nums[n - 1] + solve(nums, n - 2, dp);
+        int notTake = solve(nums, n - 1, dp);
 
-        int notTake = solve(nums, n - 1);
-
-        return max(take, notTake);
+        return dp[n] = max(take, notTake);
     } 
 
     int rob(vector<int>& nums) {
-        return solve(nums, nums.size());
+        vector<int> dp(nums.size() + 1, -1);
+
+        return solve(nums, nums.size(), dp);
     }
 };
