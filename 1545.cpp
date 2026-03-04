f09dfcd4232d0a3a0cff572ba05813ace2aae3e5
Tejasvita <ttejasvita@gmail.com>
2026-03-04 11:48:31 +0530

    1545: using Iterative Divide & Conquer (Symmetry + Bit Flip Tracking) approach
    time complexity = O(n)
    space complexity = O(1)

--- 1545.cpp
+++ 1545.cpp
@@ -4,23 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    char solve(int n, int k) {
-        if (n == 1)
-            return '0';
-        
-        if (k == (1 << (n - 1)))
-            return '1';
-        if (k < (1 << (n - 1)))
-            return solve(n - 1, k);
-        
-        char bit = solve(n - 1, (1 << n) - k);
-
-        if (bit == '1')
-            return '0';
-        return '1';
-    }
-
     char findKthBit(int n, int k) {
-        return solve(n, k);
+        bool flip = false;
+
+        while (n > 1) {
+            if (k == (1 << (n - 1))) {
+                if (flip)
+                    return '0';
+                return '1';
+            }
+
+            if (k > (1 << (n - 1))) {
+                k = (1 << n) - k;
+                flip = !flip;
+            }
+
+            n--;
+        }
+
+        if (flip)
+            return '1';
+        return '0';
     }
 };
