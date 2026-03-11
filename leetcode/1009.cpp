0b8790be8e3cae0930dd2be5d5da9cec81c28648
Tejasvita <ttejasvita@gmail.com>
2026-03-11 10:46:55 +0530

    1009: using Bit-Length Mask Complement approach

--- leetcode/1009.cpp
+++ leetcode/1009.cpp
@@ -1,16 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
+// Approach Name: Bit-Length Mask Complement
+// Time complexity = O(log n)
+// Space complexity = O(1)
 
 class Solution {
 public:
-    int bitwiseComplement(int n) {
-        if (n == 0)
-            return 1;
-        
-        int power = 0;
-        while (n >= (1 << power))
-            power++;
-        int comp = ((1 << power) - 1) - n;
-        return comp;
+  int bitwiseComplement(int n) {
+    if (n == 0)
+      return 1;
+    
+    int bits = 0;
+    int temp = n;
+    // count the number of bits in n
+    while (temp) {
+      bits++;
+      temp = temp >> 1;
     }
+    // binary number + its complimentary = all ones (upto 'bits' bits)
+    // => all ones (upto 'bits' bits) - n = ans
+    int ones = (1 << bits) - 1;
+    return (ones - n);
+  }
 };
